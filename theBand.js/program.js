f8e79fef28f21837635aa03008706ea5287bdd88
Nguyen Van Minh <minh260799@gmail.com>
2022-11-03 16:35:44 +0900

    Update Responsive with mobile and table

--- theBand.js/program.js
+++ theBand.js/program.js
@@ -16,6 +16,39 @@ for (const buyBtn of buyBtns) {
 
 modalClose.addEventListener('click', hiddenModal)
 modalOpen.addEventListener('click', hiddenModal)
-modalContainer.addEventListener('click', function(event) {
+modalContainer.addEventListener('click', function (event) {
     event.stopPropagation()
 })
+
+/* Đóng mở menu mobile */
+var headerElement = document.getElementById('header');
+
+var mobileMenu = document.getElementById('mobile-menu');
+
+var headerHight = header.clientHeight;
+mobileMenu.onclick = function () {
+    var isClose = header.clientHeight === headerHight;
+    if (isClose) {
+        headerElement.style.height = 'auto';
+    } else {
+        headerElement.style.height = null;
+    }
+}
+
+/* Tự động đóng khi chon menu */
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+
+    menuItem.onclick = function (event) {
+        /* Từ khoá this để chỉ chính thằng mình click */
+        var isParentMenu = this.nextElementSibling &&
+            this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            event.preventDefault();
+        } else {
+            header.style.height = null
+        }
+    }
+}
